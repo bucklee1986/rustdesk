08ad3fb43bded7d21e0e6226d16b97de0cb655dc
鼠标 <787870574@qq.com>
2025-08-01 10:23:33 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "10.0.0.245".to_owned()
 }
 
 #[inline]
